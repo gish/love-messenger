c56f09bbcfc85847d1db055ca3cd01dc6bb0589f
Erik Hedberg <erik.hedberg@klarna.com>
2016-06-13 21:39:51 +0200

    Add functionality for getting messages

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import sha1 from 'sha1'
+import moment from 'moment'
 import auth from './lib/auth.js'
+import getMessageList from './lib/message-list.js'
 
 const PORT = process.env.PORT || 8080
 const SALT = process.env.SALT || 'def456'
 const API_KEY = process.env.API_KEY || sha1(`${SALT}abc123`)
+const MESSAGE_RECEIVER = process.env.MESSAGE_RECEIVER
+const GOOGLE_SPREADSHEET_ID = process.env.GOOGLE_SPREADSHEET_ID || '1hqAPPlbwHrf8E8MuJ6QJSobAEliT3UFwN0xA3vp5-2Y'
+
 const app = express()
 
 // Authentication middleware
@@ -31,8 +36,22 @@ app.use(authMiddleware)
 
 // POST message
 app.post('/message', (req, res) => {
+  const todaysDate = moment().format('YYYY-MM-DD')
+
   res.status(202)
-  res.send('send message')
+  getMessageList(GOOGLE_SPREADSHEET_ID).then((messageList) => {
+    console.log(messageList)
+    res.send(messageList)
+  })
+
+  //getMessageList(googleHandler).then((list) => {
+   // message = getMessage(messageList, todaysDate)
+
+    //sendLoveMessage(MESSAGE_RECEIVER, message)
+  //})
+  //const googleHandler = undefined
+
+  //res.send('send message')
 })
 
 app.listen(PORT, () => {
