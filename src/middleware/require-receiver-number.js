f0e3c7986a8dc734a281a48aa89d3c42689a026a
Erik Hedberg <erik.hedberg@klarna.com>
2016-06-30 17:07:25 +0200

    Create middleware for validating receiver number

--- src/middleware/require-receiver-number.js
+++ src/middleware/require-receiver-number.js
@@ -1,1 +1,21 @@
-export default () => {}
+const requireReceiverNumber = (req, res, next) => {
+  const numberRegex = /^([+]46)7[0236]\d{7}$/
+  const receiverNumber = req.params['receiver_number'] || ''
+  const validNumber = numberRegex.test(receiverNumber)
+
+  if (!receiverNumber) {
+    res.status(400)
+    res.send('Missing parameter receiver_number')
+    return
+  }
+
+  if (!validNumber) {
+    res.status(400)
+    res.send('Invalid parameter receiver_number')
+    return
+  }
+
+  next()
+}
+
+export default requireReceiverNumber
