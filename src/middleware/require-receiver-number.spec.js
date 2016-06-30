f968a93e1bb497c767e2675ff4563f7dee95ffb3
Erik Hedberg <erik.hedberg@klarna.com>
2016-06-30 06:55:51 +0200

    Add test stubs

---
+++ src/middleware/require-receiver-number.spec.js
@@ -0,0 +1,15 @@
+import expect from 'expect'
+import requireReceiverNumber from './require-receiver-number'
+
+describe('Require receiver number middleware', () => {
+  describe('Invalid or missing number', () => {
+    it('should return 400 when number is not provided', () => {})
+    it('should return 400 when number is not valid', () => {})
+    it('should send message when number is not provided', () => {})
+    it('should send message when number is not valid', () => {})
+  })
+
+  describe('Valid number', () => {
+    it('should proceed when number is valid', () => {})
+  })
+})
